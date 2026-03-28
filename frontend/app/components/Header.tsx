9bebb05f8a57e928d708f2a672e4a56eac08a511
likapantrip <likapantrip@gmail.com>
2026-03-29 02:09:53 +0900

    bodyの設定修正
    ヘッダーとフッターからcontainerを削除し、layoutでmarginなどを設定

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="bg-white dark:bg-black border-b border-gray-200 dark:border-gray-600">
-      <div className="container mx-auto px-4 py-4">
+      <div className="mx-auto px-4 py-4">
         <h1 className="text-2xl font-bold text-black dark:text-zinc-50">LogLift</h1>
       </div>
     </header>
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-4xl mx-auto flex flex-col min-h-screen`}
       >
         <header>
           <Header />
